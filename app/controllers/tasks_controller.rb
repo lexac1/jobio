3f1598267e73376eaa4db4df5ee71c8cb77697c1
ffsommers <ssommers21@gmail.com>
2017-01-07 15:50:46 -0800

    adds crud routes to tasks controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,32 @@
 class TasksController < ApplicationController
   protect_from_forgery with: :exception
   
+  def index 
+
+  end
+  
+  def new 
+
+  end 
+
+  def create 
+
+  end 
+
+  def show 
+
+  end 
+
+  def edit 
+
+  end 
+
+  def update 
+
+  end 
+
+  def delete
+
+  end 	
+
 end
