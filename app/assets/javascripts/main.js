7f8d90c8fc5594e7f5be7c4d406e96f80090e2bc
ffsommers <ssommers21@gmail.com>
2017-01-06 23:44:10 -0800

    sets count down progress bar to 55 minutes in main.js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -3,8 +3,10 @@ $( document ).on('turbolinks:load', function(){
 		console.log("clicked");
 		$('#line-container').show();
         var line = new ProgressBar.Line('#line-container', {
+        strokeWidth: 3,
+    	trailWidth: 1,
         color: '#ED6A5A',
-        duration: 60000
+        duration: 3300000
        });
       line.set(1);
       line.animate(0,0);
