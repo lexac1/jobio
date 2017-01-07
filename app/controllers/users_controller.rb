b80c7d7dc21ecd13239aa4ec6961bd4e6e8d4ac3
ffsommers <ssommers21@gmail.com>
2017-01-06 21:49:00 -0800

    adds login logout to nav bar. adds redirect to users not logged in on home page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
   protect_from_forgery with: :exception
   def show 
-
+   if !current_user
+   	  redirect_to "users/sign_in"
+   end	  
   end 
 
   
