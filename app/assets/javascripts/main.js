e93f9980ca8bafe153841af5b4cd275745b7d527
Alexis Carroll <lex.ac1@gmail.com>
2017-01-11 14:49:04 -0800

    Update the modal and timer functions

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,7 @@
 $( document ).on('turbolinks:load', function(){
   hideModal();
   startBtnListener();
-  newTaskListener();
+  // newTaskListener();
   setBlockName();
   settingsBtnListener();
 
@@ -14,12 +14,11 @@ var hideModal = function() {
 }
 var startBtnListener = function() {
   $(".start").on("click", function(event){
-    // console.log("clicked");
     $(".start").hide();
     $("#settings-button").hide();
     // var fiftyFiveMinutes = 60 * 55,
-    var fiftyFiveMinutes = 2,
-    display = $('#time');
+    var fiftyFiveMinutes = 2
+    var display = document.querySelector('#time');
 
     startTimer(fiftyFiveMinutes, display);
 
@@ -27,6 +26,7 @@ var startBtnListener = function() {
 }
 
 function initializeProgressBar() {
+  $('#line-container').hide();
   $('#line-container').show();
   var line = new ProgressBar.Line('#line-container', {
     strokeWidth: 4,
@@ -40,7 +40,7 @@ function initializeProgressBar() {
 
 function startTimer(duration, display) {
   var timer = duration, minutes, seconds;
-  setInterval(function () {
+  var interval = setInterval(function () {
     minutes = parseInt(timer / 60, 10);
     seconds = parseInt(timer % 60, 10);
 
@@ -51,36 +51,41 @@ function startTimer(duration, display) {
     initializeProgressBar();
 
     if (--timer < 0) {
-      timer = duration,
+      timer = duration;
+
       $("#myModalNorm").modal('show')
-      setBlockName()
+      $(".modal-footer").on('click', function() {
+        setBlockName();
+        $("#myModalNorm").modal('toggle')
+        $('#line-container').empty()
+        initializeProgressBar();
+      })
     }
   }, 1000);
 }
 
-var newTaskListener = function() {
-  $("#new_task").on("submit", function() {
-    event.preventDefault();
-
-    var form = $(this);
-    var method ="PUT"
-    var url = "/tasks/1"
-    var data = $(form).serialize();
-    console.log('newtasklistener')
-    $.ajax({
-      method: method,
-      url: url,
-      data: data
-    })
-  })
-}
+// var newTaskListener = function() {
+//   $("#new_task").on("click", function() {
+//     event.preventDefault();
+//
+//     var form = $(this);
+//     var method ="PUT"
+//     var url = "/tasks/1"
+//     var data = $(form).serialize();
+//     console.log('newtasklistener')
+//     $.ajax({
+//       method: method,
+//       url: url,
+//       data: data
+//     })
+//   })
+// }
 
 var settingsBtnListener = function() {
   $("#settings-button").on("click", function(event){
     event.preventDefault();
     $(document).ready(ready);
     $("#settings-button").hide();
-    // console.log("clicked settings");
     $("#main-page").css("display", "none");
     $("#settings").show();
     $("#settings").addClass("animated slideInUp");
@@ -97,21 +102,6 @@ var formBtnListener = function () {
     })
   })
 }
-
-// var changeBlockName = function() {
-//   var blocks = $('#blocks').children();
-//   var blockDisplayName = $('#block-name').html()
-//   var blockNames = [];
-//
-//   for (var i = 0; i < blocks.length; i++) {
-//     blockNames.push($(blocks[i]).html())
-//   }
-//
-//   var currentIndex = blockNames.indexOf(blockDisplayName)
-//   console.log(currentIndex)
-//
-//   var nextBlockTitle = $('#block-name').html(blockNames[currentIndex + 1])
-// }
 
 var setBlockName = function() {
   if (incriment === 5) {
