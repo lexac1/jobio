c6ada2eb242256db06deadb07310bca9d5a26359
ffsommers <ssommers21@gmail.com>
2017-01-13 13:33:19 -0800

    adds conditional to users show where if a new user signs up and they have no blocks it creates the default blocks for the user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,18 @@ class UsersController < ApplicationController
   def index
 
   end
+   
 
   def show
     @user = User.find(current_user.id)
     @blocks = @user.blocks
+    if !@blocks.any? 
+        Block.create(category_id: 1, due: 'Sat, 19 Nov 2011 18:54:13', priority: 1, user_id: @user.id)
+        Block.create(category_id: 2, due: 'Sat, 19 Nov 2011 18:54:13', priority: 2, user_id: @user.id)
+        Block.create(category_id: 3, due: 'Sat, 19 Nov 2011 18:54:13', priority: 3, user_id: @user.id)
+        Block.create(category_id: 4, due: 'Sat, 19 Nov 2011 18:54:13', priority: 4, user_id: @user.id)
+        Block.create(category_id: 5, due: 'Sat, 19 Nov 2011 18:54:13', priority: 5, user_id: @user.id)  
+     end   
     @tasks = Task.where(user_id: @user.id)
     @task = @user.tasks.new
   end
